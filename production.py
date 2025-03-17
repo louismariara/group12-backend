ba82d965e5c6e3effc2ee10bc336f14e0a219c8f
Louis Mariara <louismariara@gmail.com>
2025-03-17 18:24:50 +0300

    confirm urls

--- production.py
+++ production.py
@@ -6,7 +6,7 @@ class ProductionConfig(Config):
     DEBUG = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     SECRET_KEY = os.getenv('SECRET_KEY')
-    CORS_ORIGINS = os.getenv('CORS_ORIGINS', 'https://your-production-frontend.com').split(',')
+    CORS_ORIGINS = os.getenv('CORS_ORIGINS', 'https://group12-frontend.vercel.app/').split(',')
     CORS_RESOURCES = {r"/api/*": {"origins": CORS_ORIGINS}}
     SQLALCHEMY_ECHO = False  # Explicitly disable query logging
     logging.basicConfig(filename='app.log', level=logging.INFO)  
